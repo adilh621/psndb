80b8f4c8554302b947e6867eb37a14fb28d238f7
Adil Hussain <adil@Adils-MacBook-Pro.local>
2023-02-08 09:39:44 -0500

    pushing nextjs blank app, about to work

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+
+
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  // env:{
+  //   mapbox_key:'pk.eyJ1IjoiYWRpbGg2MjEiLCJhIjoiY2xjeG50MmxlMWZvcDN3cGw4b3J3eGp2OCJ9.DxgwuwpCLpH28zgs7YiFZQ'
+  // }
+}
